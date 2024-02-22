605cd84a957f6f97c5ea3d0b9a6f0373e49062ac
Vagelis Papadimitriou <papadimitriou.vg@gmail.com>
2024-02-22 23:53:21 +0200

    Add project files.

---
+++ Models/Entities/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentCRUD.Models.Entities
+{
+    public class Student
+    {
+        public Guid StudentId { get; set; }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Subscribed { get; set; }
+    }
+}
---
+++ MyDatabase/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentCRUD.Models.Entities;
+
+namespace StudentCRUD.MyDatabase
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
